564515386607f59ca1bfba11c51fd7333895ba24
Lucas Ramsey <lucas.r.ramsey@gmail.com>
2017-09-22 11:12:47 -0500

    added encryption

---
+++ main.py
@@ -0,0 +1,55 @@
+from flask import Flask, request
+from caesar import rotate_string
+import cgi
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+form = """
+    <!DOCTYPE html>
+
+    <html>
+        <head>
+            <style>
+                form {
+                    background-color: #eee;
+                    padding: 20px;
+                    margin: 0 auto;
+                    width: 540px;
+                    
+                    font: 16px sans-serif;
+                    border-radius: 10px;
+                }
+                textarea {
+                    margin: 10px 0;
+                    width: 540px;
+                    height: 120px;
+                }
+            </style>
+        </head>
+        <body>
+            <form method='POST'>
+                <label for="rotate_by">Rotate by: </label>
+                <input id="rotate_by" type="text" name="rot" value="0"  />
+                <textarea name="text"></textarea>
+                <input type="submit" value="Submit querry">
+            </form>
+        </body>
+    </html>
+"""
+
+@app.route("/")
+def index():
+    return form
+
+@app.route("/", methods=['POST'])
+def encrypt():
+    rot_1st = cgi.escape(request.form["rot"])
+    text = cgi.escape(request.form["text"])
+    rot = int(rot_1st)
+
+    encrypted_text = rotate_string(text, rot)
+    content = '<h1>' + encrypted_text + '</h1>'
+    return content
+
+app.run()
